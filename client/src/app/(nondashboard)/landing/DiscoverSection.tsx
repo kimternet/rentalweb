75c765a01e271da9d4bb8426f141d5f38dce45a3
kimternet <kimternet23@gmail.com>
2025-04-11 02:54:23 +0900

    backend 서버 구축, prisma db 설정

---
+++ client/src/app/(nondashboard)/landing/DiscoverSection.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import React from 'react'
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.2,            
+        },
+    },
+};
+
+const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+};
+
+
+
+const DiscoverSection = () => {
+  return (
+    <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.8 }}
+        variants= {containerVariants}
+        className="py-12 bg-white mb-16"
+    >
+        <div className="max-w-6xl xl:max-w-7xl mx-auto px-6 sm:px-8 lg:px-12 xl:px-16">
+            <motion.div
+                variants={itemVariants}
+                className="my-12 text-center"
+                
+            >
+                <h2 className="text-3xl font-semibold leading-tight text-gray-800">
+                    Discover
+                </h2>
+                <p className="mt-4 text-lg text-gray-600">
+                    Find your Dream Rental Property Today!
+                </p>
+                <p className="mt-2 text-gray-500 max-w-3xl mx-auto">
+                    Explore our wide range of rental properties and find your perfect match.
+                    Our platform is designed to make your rental search easy and efficient.
+                    Start your journey to finding your dream rental property today!
+                </p>
+            </motion.div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 lg:gap-12 xl:gap-16 text-center">
+                {[
+                    {
+                        imageSrc: "/landing-icon-wand.png",
+                        title: "Search for Properties",
+                        description: "Use our advanced search filters to find the perfect rental property for your needs.",
+                    },
+                    {
+                        imageSrc: "/landing-icon-calendar.png",
+                        title: "Book Your Rental",
+                        description: "Book your rental property with ease and convenience.",
+                    },
+                    {
+                        imageSrc: "/landing-icon-heart.png",
+                        title: "Enjoy your New Home",
+                        description: "Experience the joy of living in your new rental property.",
+                    },
+                ].map((card, index) => (
+                    <motion.div key={index} variants={itemVariants}>
+                        <DiscoverCard {...card} />
+                    </motion.div>
+                ))}
+            </div>
+
+        </div>
+            
+    </motion.div>
+  );
+};
+
+const DiscoverCard = ({
+    imageSrc,
+    title,
+    description,
+
+}: {
+    imageSrc: string;
+    title: string;
+    description: string;
+
+}) => (
+    <div className="px-4 py-12 shadow-lg rounded-lg bg-primary-50 md:h-72">
+        <div className="bg-primary-700 p-[0.6rem] rounded-full mb-4 h-10 w-10 mx-auto">
+            <Image
+                src={imageSrc}
+                alt={title}
+                width={30}
+                height={30}
+                className="w-full h-full"
+            />
+        </div>
+        <h3 className="mt-4 text-xl font-medium text-gray-800">{title}</h3>
+        <p className="mt-2 text-base text-gray-500">{description}</p>
+    </div>
+)
+
+export default DiscoverSection;
