c30f37afab56a2e2d05d95003f533f26d01223ee
kimternet <kimternet23@gmail.com>
2025-04-16 13:50:46 +0900

    로그아웃, 설정, 대시보드 페이지, Sidebar 추가

---
+++ client/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import Navbar from '@/components/Navbar';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import Sidebar from "@/components/AppSidebar";
+import { NAVBAR_HEIGHT } from '@/lib/constants';
+import React, { useEffect, useState } from 'react'
+import { useGetAuthUserQuery } from '@/state/api';
+import { useRouter, usePathname } from 'next/navigation';
+
+
+
+const DashboardLayout = ({children}: {children: React.ReactNode}) => {
+
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (authUser) {
+        const userRole = authUser.userRole?.toLowerCase();
+        if (
+            (userRole === "manager" && pathname.startsWith("/tenants")) ||
+            (userRole === "tenant" && pathname.startsWith("/managers"))
+        ) {
+           router.push(
+            userRole === "manager"
+            ? "/managers/properties"
+            : "/tenants/favorites"
+           );
+        } else {
+            setIsLoading(false);
+        }
+    }
+  }, [authUser, router, pathname]);
+
+  if (authLoading || isLoading) return <>Loading...</>
+
+  if (!authUser?.userRole) return null;
+
+  return (
+    <SidebarProvider>
+    <div className="min-h-screen w-full bg-primary-100">
+        <Navbar />
+        <div style={{ paddingTop: `${NAVBAR_HEIGHT}PX`}}>
+          <main className="flex">
+              <Sidebar userType={authUser?.userRole.toLowerCase()} />
+              <div className="flex-grow transition-all duration-300">
+                  {children}
+              </div>
+          </main>
+
+        </div>
+    </div>
+    </SidebarProvider>
+  );
+};
+
+export default DashboardLayout;
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { NAVBAR_HEIGHT } from '@/lib/constants';
 import Link from 'next/link';
 import Image from 'next/image';
