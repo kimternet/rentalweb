8286b3766044068f9db5e613a040e11edd5ec342
kimternet <kimternet23@gmail.com>
2025-04-22 12:57:36 +0900

    backend application and lease routes 완료

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,8 @@ import { authMiddleware } from "./middleware/authMiddleware";
 import tenantRoutes from "./routes/tenantRoutes";
 import managerRoutes from "./routes/managerRoutes";
 import propertyRoutes from "./routes/propertyRoutes";
+import leaseRoutes from "./routes/leaseRoutes";
+import applicationRoutes from "./routes/applicationRoutes";
 
 
 /* CONFIGURATIONS */
@@ -28,8 +30,10 @@ app.get("/", (req, res) => {
     res.send("home route");
 });
 
+app.use("/applications", applicationRoutes);
 app.use("/properties", propertyRoutes);
-app.use("/tenents", authMiddleware(["tenant"]), tenantRoutes);
+app.use("/leases", leaseRoutes);
+app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
 app.use("/managers", authMiddleware(["manager"]), managerRoutes);
 
 /* SERVER */
